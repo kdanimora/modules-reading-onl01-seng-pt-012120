6c57f6340fa6d16f73fb964b83a0cd056500b34f
Karen Daniela <kdaniimora@gmail.com>
2020-02-24 17:36:25 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,7 +3,7 @@ require_relative './class_methods_module.rb'
 
 class Kid  
   extend FancyDance::MetaDancing 
-  include FancyDance:: 
+  include FancyDance::InstanceMethods
   
   attr_accessor :name 
   
